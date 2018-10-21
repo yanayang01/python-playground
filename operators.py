26d3123e117fbcb627529cc794ab11eb235c1819
Yana Yang <yana.yang@gmail.com>
2018-10-21 16:00:11 -0400

    updated files to include more examples

--- operators.py
+++ operators.py
@@ -15,11 +15,9 @@ def loop_test1():
     while a < N:
         print("%d" % a)
         a *= a  # equivalent tp a = a * a
-        
 
-
+# geometric sum of 1/x + 1/(x+1) + 1/(x+2) + ... + 1/n
 def loop_test2(n, x):
-    # geometric sum of 1/x + 1/(x+1) + 1/(x+2) + ... + 1/n
     sum = 0.0
     if x < n:
         print(f"For x = {x} and n = {n} the geometric sum is:")
@@ -29,7 +27,7 @@ def loop_test2(n, x):
     else:
         print("x must be less than n!!!")
 
-
+# function to get the user input for the geometric loop calculation
 def input_into_loop_test2():
     n = int(input("Enter an integer: "))
     print(f"n = {n}")
@@ -38,6 +36,19 @@ def input_into_loop_test2():
     loop_test2(n, x)
 
 
+# function to evaluate the quadratic equation
+def quadratic_test(a, b, c):
+    import math
+    d = b * b - 4 * a * c
+    print(f"For a = {a}, b = {b}, and c = {c}:")
+    if d < 0:
+        print("Your roots are imaginary gurl!")
+    else:
+        root1 = (-b + math.sqrt(d)) / (2.0 * a)
+        root2 = (-b - math.sqrt(d)) / (2.0 * a)
+        print(f"Your roots are: {root1:.4f} and {root2:.4f}!")
+
 # loop_test1()
 # loop_test2(10, 1)
 # input_into_loop_test2()
+# quadratic_test(30, 4, 1)
