8a823cadfab3c8ab5820e2f5a67aa127dad22cf6
Yana Yang <yana.yang@gmail.com>
2018-10-21 16:10:59 -0400

    small update to file to include smileys

--- if_statements.py
+++ if_statements.py
@@ -1,1 +1,13 @@
 # file to mess around with if statements in python
+
+# multiple if else example
+def if_else_test():
+    x = int(input("Enter a number: "))
+    if x < 0:
+        print("Your number is negative :(")
+    elif x > 0:
+        print("Your number is positive :D")
+    else:
+        print("Your number is zero!")
+
+if_else_test()
