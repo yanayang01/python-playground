20a1353dff39100254f05385625b3893eaa59387
Yana Yang <yana.yang@gmail.com>
2018-10-22 20:02:46 -0400

    game of sticks using while loop

---
+++ game_of_sticks.py
@@ -0,0 +1,27 @@
+# game of sticks using loops
+
+# example from online
+def sticks_game1():
+    sticks = 21 
+    print("There are 21 sticks, you can take 1 to 4 sticks at a time.")
+    print("Whoever takes the last stick loses!")
+
+    while sticks > 0:
+        print(f"Sticks left: {sticks}")
+        
+        sticks_taken = int(input("How many sticks are you taking? "))
+    
+        if sticks_taken > 4 or sticks_taken < 1:
+            print("You must take 1, 2, 3, or 4 sticks!")
+            continue        
+        
+        if sticks == 1:
+            print("You took the last stick, you lose sucka!")
+            break
+        
+        print(f"Computer took {5 - sticks_taken}.", "\n")
+        
+        sticks -= 5
+# whoever goes first loses
+
+sticks_game1()
